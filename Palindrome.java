a6ec83ff7df4fb20638a056c602234453e38e2f8
ranjithajulad-cpu <ranjithajulad@gmail.com>
2026-09-06 10:39:59 +0530

    Implement palindrome check in Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n=x;
+        int rev=0;
+        while(n>0){
+            int rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+            
+        }
+        return rev==x;
+    }
+   
+}
