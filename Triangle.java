68d96f4bb15fdba5f05b0b9df2b88b6c2f0c5078
ranjithajulad-cpu <ranjithajulad@gmail.com>
2026-09-09 21:51:20 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        int n = triangle.size();
+        int[][] a = new int[n][n];
+
+        // Convert List to array
+        int r = 0;
+        for (List<Integer> row : triangle) {
+            int c = 0;
+            for (int x : row) {
+                a[r][c] = x;
+                c++;
+            }
+            r++;
+        }
+
+        // Bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+
+                a[i][j] = a[i][j] + Math.min(a[i + 1][j],a[i + 1][j + 1]);
+            }
+        }
+
+        return a[0][0];
+    }
+}
